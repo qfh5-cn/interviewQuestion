4d46851a3f9261721635a5e81fbc6b51d390913e
laoxie <jeroo@qq.com>
2019-10-17 12:53:22 +0800

    优化添加面试题功能

--- src/api/index.js
+++ src/api/index.js
@@ -5,8 +5,8 @@ let pending = {};
 
 const apiserver = axios.create({
     // cancelToken: source.token,
-    // baseURL:'http://localhost:3000',
-    baseURL:'http://192.168.0.105:3000',
+    baseURL:'http://localhost:3000',
+    // baseURL:'http://192.168.0.105:3000',
 })
 
 export async function get(url,params,config={}){
--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component,createRef } from "react";
 import { connect } from "react-redux";
 import {
   Button,
@@ -13,115 +13,137 @@ import {
   Form,
   Rate
 } from "antd";
-import MyTags from '@@/MyTags';
+import MyTags from "@@/MyTags";
 
 import Api from "@/api";
 import { withAuth } from "@/utils";
 import MyEditor from "@@/MyEditor";
-import {stateToHTML} from 'draft-js-export-html';
-import ReEditor from 're-editor';
-import 're-editor/lib/styles/index.css';
+import { stateToHTML } from "draft-js-export-html";
+import ReEditor from "re-editor";
+import "re-editor/lib/styles/index.css";
 
 @withAuth
 @Form.create({ name: "addIQ" })
 class Add extends Component {
   state = {
-    interviewQuestions: "",
-    result: [],
     category: [],
-    addType: "single",
-    tags:[],
+    addType: "multiple",
+    iqs:[]
+    // tags: []
     // detail:{},
   };
+
+  // 获取元素节点
+  // iqs = createRef();
   onChange = e => {
-    let interviewQuestions = e.currentTarget.value;
+    let iqs = e.currentTarget.value;
+
+    // 监听输入框修改，同步面试题结果
+    iqs = this.formatData(iqs);
     this.setState({
-      interviewQuestions
-      //   result: this.formatData(interviewQuestions)
+      iqs
     });
   };
   changeCategory = (idx, { key, item }) => {
-    let result = this.state.result.map((iq, i) => {
+    let {iqs} = this.state;
+    iqs = iqs.map((iq, i) => {
       if (i === idx) {
         iq.category = key;
       }
       return iq;
     });
     this.setState({
-      result
+      iqs
     });
   };
   changeAddType = e => {
-    console.log(e);
     this.setState({
       addType: e.target.value
     });
   };
   formatData(data) {
-    // string -> array
+    // 格式化数据：string -> array
     if (!data) return [];
     data = data.trim().replace(/^[\*\d\s]+[、\\，\,\.]?|[？\?。\.；\;]$/gm, "");
     return data.split("\n").map(item => ({
       question: item,
-      category: "",
-      hot: 1
     }));
   }
+  removeIQ = idx=>{console.log(idx)
+    let {iqs} = this.state;
+    let {form} = this.props;
+    iqs = iqs.filter((item, i) => i != idx);
+    this.setState({
+      iqs
+    });
+
+    // 同步输入框内容
+    form.setFieldsValue({iqs:iqs.map(item=>item.question).join('\n')})
+
+  }
   // data:Array
-  addIQ = async (data) => {
+  addIQ = async iqs => {
     let { user } = this.props;
+
     let result = await Api.post("/iq", {
       userid: user._id,
-      iq: data
+      iqs
     });
-    if(result.status===200){
-      message.success("添加面试题成功",()=>{
-        this.forceUpdate()
+    if (result.status === 200) {
+      message.success("添加面试题成功", () => {
+        this.forceUpdate();
       });
-    }else{
+    } else {
       message.error("添加失败");
     }
-
-    
   };
   // 批量添加
-  add_multiple = ()=>{
-    let {result} = this.state;
-    if(result.length===0){
-      message.error('请输入面试题');
-      this.refs.iq.focus();
-      return 
-    }
-    this.addIQ(result);
-    this.setState({
-      interviewQuestions: ""
-    });
-    this.refs.iq.focus();
-  }
-  // 单条添加
-  add_single = (data)=>{
-    let {form,dispatch,history,location} = this.props;
+  add_multiple = () => {
+    let { form } = this.props;
     form.validateFieldsAndScroll(async (err, values) => {
       if (!err) {
-        let { question,detail,difficulty } = values;
-        let {tags} = this.state;
-        detail = detail.toJSON();
-        console.log(question,difficulty,tags);
-        this.addIQ([{question,detail,difficulty,tags}]);
+        let { iqs } = this.state;
+        let {company} = values;
+        if(company){
+          iqs = iqs.map(item=>{
+            item.company = company;
+            return item;
+          })
+        }
+        this.addIQ(iqs);
+      }else{
+
+        // 设置后无法报错
+        // form.setFieldsValue({iqs:''})
+        this.iqs.focus();
       }
     });
-  }
+
+  };
+  // 单条添加
+  add_single = () => {
+    let { form} = this.props;
+    form.validateFieldsAndScroll(async (err, values) => {
+      if (!err) {
+        values.detail = values.detail.toJSON();
+        console.log("single:", values);
+        this.addIQ([{ ...values }]);
+      }else{
+        
+      }
+    });
+  };
   getCategoryName = code => {
     if (!code) return "设置分类";
     let current = this.state.category.filter(item => item.code == code)[0];
     return current.name;
   };
 
-  changeTags = tags =>{
+  changeTags = tags => {
     this.setState({
       tags
-    })
-  }
+    });
+  };
 
   async componentDidMount() {
     //获取分类
@@ -141,28 +163,39 @@ class Add extends Component {
   }
   componentWillUpdate(nextProps, nextState) {
     // console.log("componentWillUpdate:", nextState);
-    // 输入框与列表数据映射处理
-    if (nextState.interviewQuestions != this.state.interviewQuestions) {
-      // 如何实现保留result原有数据（如category）
-      let newData = this.formatData(nextState.interviewQuestions);
-      this.setState({
-        result: newData
-      });
-    }
+    // 输入框与结果列表数据映射处理
+    // let {form} = this.props;
+    // let {result} = this.state;
+    // let {form:nextForm} = nextProps
+    // let {result:nextResult} = nextState;
 
-    if (nextState.result.length != this.state.result.length) {
-      this.setState({
-        interviewQuestions: nextState.result
-          .map(item => item.question)
-          .join("\n")
-      });
-    }
+    // let nextIQs = nextForm.getFieldValue('iqs');
+    // let iqs = form.getFieldValue('iqs');console.log('nextIQs:iqs',nextIQs,iqs)
+    // if (nextIQs != iqs) {
+    //   // 如何实现保留result原有数据（如category）
+    //   let result = this.formatData(nextIQs);
+    //   this.setState({
+    //     result
+    //   });
+    // }
+    
+
+    // if (nextResult.length != result.length) {
+    //   let iqs = result.map(item=>item.question).join('\n')
+    //   form.setFieldsValue({iqs})
+    // }
   }
   componentDidUpdate(nextProps, nextState) {
     // console.log("componentDidUpdate:", nextState);
   }
   render() {
-    let { interviewQuestions, result, category, addType,detail } = this.state;
+    let {
+      iqs,
+      category,
+      addType,
+      detail,
+      showCompany,
+    } = this.state;
 
     let { getFieldDecorator, getFieldValue } = this.props.form;
 
@@ -177,80 +210,69 @@ class Add extends Component {
           <Radio value="multiple">批量添加</Radio>
           <Radio value="single">普通添加</Radio>
         </Radio.Group>
-        {addType === "multiple" ? (
-          <div className="multiple">
-            <Input
-              prefix={
-                <Icon type="link" style={{ color: "rgba(0,0,0,.25)" }} />
-              }
-              suffix={
-                <Tooltip title="面试题目用于显示，请尽量精简">
-                  <Icon
-                    type="info-circle"
-                    style={{ color: "rgba(0,0,0,.45)" }}
-                  />
-                </Tooltip>
-              }
-            />
-            <Input.TextArea
-              placeholder="输入面试题，每行一道"
-              value={interviewQuestions}
-              onChange={this.onChange}
-              autosize={{ minRows: 5 }}
-              ref="iq"
-            ></Input.TextArea>
-            {/* 自动识别数据（分阶段） */}
-            <List
-              style={{ marginTop: 15 }}
-              itemLayout="vertical"
-              size="small"
-              dataSource={result}
-              renderItem={(item, idx) => (
-                <List.Item
-                  key={item.question}
-                  extra={[
-                    <Dropdown
-                      overlay={
-                        <Menu onClick={this.changeCategory.bind(this, idx)}>
-                          {category.map(item => (
-                            <Menu.Item key={item.code}>{item.name}</Menu.Item>
-                          ))}
-                        </Menu>
-                      }
-                      key="menu"
-                    >
-                      <Button size="small">
-                        {this.getCategoryName(item.category)}
-                        <Icon type="down" />
-                      </Button>
-                    </Dropdown>,
-                    <Button
-                      key="btn"
-                      type="link"
-                      icon="close"
-                      size="small"
-                      shape="circle"
-                      style={{ marginLeft: 20 }}
-                      onClick={() => {
-                        result = result.filter((item, i) => i != idx);
-                        console.log("result:", result);
-                        //   interviewQuestions = result.join('\n');
-                        this.setState({
-                          result
-                          //   interviewQuestions
-                        });
-                      }}
-                    ></Button>
-                  ]}
-                >
-                  {idx + 1} - {item.question}
-                </List.Item>
+        <Form layout="vertical">
+          {showCompany ? (
+            <Form.Item label="公司名称">
+              {getFieldDecorator("company", {})(
+                <Input
+                  prefix={<span style={{ color: "rgba(0,0,0,.25)" }}>@</span>}
+                  suffix={
+                    <Tooltip title="在哪家公司被问到该面试题，请尽量填写，可以帮助到更多的人">
+                      <Icon
+                        type="info-circle"
+                        style={{ color: "rgba(0,0,0,.45)" }}
+                      />
+                    </Tooltip>
+                  }
+                  addonAfter={
+                    <Tooltip title="不填写公司">
+                      <Icon
+                        type="close"
+                        style={{color:'#f00'}}
+                        onClick={() => {
+                          this.setState({ showCompany: false });
+                        }}
+                      />
+                    </Tooltip>
+                  }
+                />
               )}
-            />
-          </div>
-        ) : (
-          <div className="single">
-            <Form layout="vertical">
+            </Form.Item>
+          ) : (
+            <div className="ant-form-item">
+              <Button
+                type="dashed"
+                icon="plus"
+                onClick={() => {
+                  this.setState({ showCompany: true });
+                }}
+              >
+                添加公司
+              </Button>
+            </div>
+          )}
+
+          {addType === "multiple" ? (
+            <>
+              <Form.Item label="面试题（每行一道）">
+                {getFieldDecorator("iqs", {
+                  initialValue:iqs.map(item=>item.question).join('\n'),
+                  rules:[
+                    {required:true,message:'请输入面试题'}
+                  ]
+                })(
+                  <Input.TextArea
+                    placeholder="输入面试题，每行一道"
+                    onChange={this.onChange}
+                    autosize={{ minRows: 5 }}
+                    ref={el=>this.iqs=el}
+                    // ref={this.iqs}
+                  />
+                )}
+              </Form.Item>
+            </>
+          ) : (
+            <>
               <Form.Item label="面试题目">
                 {getFieldDecorator("question", {
                   rules: [
@@ -277,36 +299,75 @@ class Add extends Component {
               </Form.Item>
               <Form.Item label="难度">
                 {getFieldDecorator("difficulty", {
-                  initialValue:1,
-                })(
-                  <Rate/>
-                )}
+                  initialValue: 1
+                })(<Rate />)}
               </Form.Item>
               <Form.Item label="标签">
                 {getFieldDecorator("tags", {
-                  initialValue:1,
-                })(
-                  <MyTags onChange={this.changeTags}/>
-                )}
+                  initialValue: 1
+                })(<MyTags onChange={this.changeTags} />)}
               </Form.Item>
               <Form.Item label="问题补充（选填）">
                 {getFieldDecorator("detail", {
-                  initialValue:detail,
-                  rules: []
-                })(<ReEditor onChange={(detail)=>{
-                  this.setState({
-                    detail
-                  })
-                }} />)}
+                  initialValue: detail
+                })(
+                  <ReEditor
+                    onChange={detail => {
+                      this.setState({
+                        detail
+                      });
+                    }}
+                  />
+                )}
               </Form.Item>
-            </Form>
-          </div>
-        )}
+            </>
+          )}
+        </Form>
+        {/* 自动识别数据（分阶段） */}
+        {addType === "multiple" ? (
+          <List
+            style={{ margin: '15px 0' }}
+            itemLayout="vertical"
+            size="small"
+            dataSource={iqs}
+            renderItem={(item, idx) => (
+              <List.Item
+                key={item.question}
+                extra={[
+                  <Dropdown
+                    overlay={
+                      <Menu onClick={this.changeCategory.bind(this, idx)}>
+                        {category.map(item => (
+                          <Menu.Item key={item.code}>{item.name}</Menu.Item>
+                        ))}
+                      </Menu>
+                    }
+                    key="menu"
+                  >
+                    <Button size="small" type="link">
+                      {this.getCategoryName(item.category)}
+                      <Icon type="down" />
+                    </Button>
+                  </Dropdown>,
+                  <Tooltip title="删除" key="btn">
+                    <Icon
+                      
+                      type="close-circle"
+                      style={{ marginLeft: 20,color:'#f00' }}
+                      onClick={this.removeIQ.bind(this,idx)}
+                    />
+                  </Tooltip>
+                ]}
+              >
+                {idx + 1} - {item.question}
+              </List.Item>
+            )}
+          />
+        ) : null}
         <Button
           size="large"
           type="primary"
-          style={{ marginTop: 15 }}
-          onClick={this['add_'+addType]}
+          onClick={this["add_" + addType]}
         >
           添加
         </Button>
